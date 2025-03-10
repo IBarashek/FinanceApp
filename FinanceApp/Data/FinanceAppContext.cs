6d129d6d5231df7942db5f17805ad8402a37d0fc
IBarashek <yuphighlight@mail.ru>
2025-03-10 16:21:06 +0300

    aaaaaaaaa

---
+++ FinanceApp/Data/FinanceAppContext.cs
@@ -0,0 +1,17 @@
+﻿using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+namespace FinanceApp.Data
+{
+    public class FinanceAppContext: DbContext
+    {
+        //Промежуточный класс между БД и данными в приложении
+        // Добавлены Nuget MicrosoftEntityFrameworkCore, MicrosoftEntityFrameworkCoreSqlServer, MicrosoftEntityFrameworkCoreTools
+        // Нужно для миграции бд
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base(options){ }
+        public DbSet<Expense> Expenses { get; set; }
+
+
+
+    }
+}
